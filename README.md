efa396358e1c2a87c9198bafc90f0927efcd2ac1
Shahid Kamal <shahidkamal08@gmail.com>
2020-08-12 01:49:44 +0530

    added docs

---
+++ README.md
@@ -0,0 +1,5 @@
+### msg91.js is wrapper around msg91 apis.
+
+#### Docs
+
+Docs are generated with the help of [TypeDoc](http://typedoc.com) and can be found here [Doc](/docs)
--- src/otp.ts
+++ src/otp.ts
@@ -10,16 +10,40 @@ import { toQueryParams } from './utils';
 import debug from 'debug';
 
 const Logger = debug('MSG91');
-
+/**
+ * OTP Class with `send`, `verify` and `resend`
+ * ```
+ * const OTP = new MSG91.OTP({
+ *  authKey,
+ * });
+ * ```
+ */
 export class OTP {
   options: OTPOptions;
 
   host: string;
 
+  /**
+   * @param options Library Options
+   */
   constructor(options: OTPOptions) {
     this.options = options;
     this.host = `${Constants.HOST}`;
   }
+
+  /**
+   * Send otp api from msg91
+   * Example:
+   * ```javascript
+   * const OTP = new MSG91.OTP(options);
+   * const sendOTPParams={
+   *  template_id: '',
+   *  mobile: '',
+   * }
+   * const sendOTP = OTP.send(sendOTPParams)
+   * ```
+   * @param sendOTPParams send otp params
+   */
 
   async send(sendOTPParams: SendOTPParams): Promise<Response> {
     const query = toQueryParams(sendOTPParams);
@@ -35,7 +59,19 @@ export class OTP {
     const jsonResult = JSON.parse(result);
     return jsonResult;
   }
-
+  /**
+   * Verify otp api from msg91
+   * Example:
+   * ```javascript
+   * const OTP = new MSG91.OTP(options);
+   * const verifyOTPParams={
+   *  otp: '',
+   *  mobile: '',
+   * }
+   * const verifyOTP = OTP.verify(verifyOTPParams)
+   * ```
+   * @param verifyOTPParams verify otp params
+   */
   async verify(verifyOTPParams: VerifyOTPParams): Promise<Response> {
     const query = toQueryParams(verifyOTPParams);
     const url = `${this.host}/otp/verify?authkey=${this.options.authKey}&${query}`;
@@ -50,6 +86,20 @@ export class OTP {
     const jsonResult = JSON.parse(result);
     return jsonResult;
   }
+
+  /**
+   * Resent otp api from msg91
+   * Example:
+   * ```javascript
+   * const OTP = new MSG91.OTP(options);
+   * const resentOTPParams={
+   *  otp: '',
+   *  mobile: '',
+   * }
+   * const resentOTP = OTP.resend(resentOTPParams)
+   * ```
+   * @param resentOTPParams resend otp params
+   */
 
   async resend(resendOTPParams: ResendOTPParams): Promise<Response> {
     const query = toQueryParams(resendOTPParams);
